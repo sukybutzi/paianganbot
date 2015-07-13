4ee8f3fa717877b5afa9de706f2b786a745243cd
aalinnfs <aalinnfs@gmail.com>
2015-07-13 06:35:59 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -378,9 +378,9 @@
                     var pos = Math.floor((Math.random() * API.getWaitList().length) + 1);
                     var user = basicBot.userUtilities.lookupUser(winner);
                     var name = user.username;
-                    API.sendChat(subChat(basicBot.chat.winnerpicked, {name: name, position: pos}));
+                    API.sendChat(subChat(basicBot.chat.winnerpicked, {name: name, position: 1}));
                     setTimeout(function (winner, pos) {
-                        basicBot.userUtilities.moveUser(winner, pos, false);
+                        basicBot.userUtilities.moveUser(winner, 1, false);
                     }, 1 * 1000, winner, pos);
                 }
             }
